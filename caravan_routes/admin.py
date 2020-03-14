48727bf8a54e22721754ceaaa958a9c916e27cb4
folomkin <bambruysk@gmail.com>
2020-03-14 23:27:27 +0300

    Добавил опсаиние отобрадение караван в админку

--- caravan_routes/admin.py
+++ caravan_routes/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from caravan_routes.models import GeoPoint, RoutePoint, Route, GeoMap
+from caravan_routes.models import GeoPoint, RoutePoint, Route, GeoMap, Caravan
 
 admin.site.register(GeoPoint)
 admin.site.register(RoutePoint)
 admin.site.register(Route)
 admin.site.register(GeoMap)
+admin.site.register(Caravan)
